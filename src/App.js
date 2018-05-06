ef295b4990bd81d5c0e33d59fb46650b6377906d
Sergey Badulin <sbadulin@gmail.com>
2018-05-07 02:42:53 +0300

    add inputs

--- src/App.js
+++ src/App.js
@@ -1,16 +1,43 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Button, Row, Col } from 'antd';
-import { SSL_OP_PKCS1_CHECK_1 } from 'constants';
+import { Button, Row, Col, Input, Icon } from 'antd';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      totalUsers: 3,
+      userName: ''
+    };
+  }
+  emitEmpty = () => {
+    this.userNameInput.focus();
+    this.setState({ userName: '' });
+  };
+  onChangeUserName = e => {
+    this.setState({ userName: e.target.value });
+  };
   render() {
+    const { userName, totalUsers } = this.state;
+    let inputs = [];
+    for (let i = 0; i < totalUsers; i++) {
+      inputs.push(
+        <Input
+          placeholder="Ссылка на пользователя"
+          prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+          value={userName}
+          onChange={this.onChangeUserName}
+          ref={node => (this.userNameInput = node)}
+        />
+      );
+    }
     return (
       <div className="App">
         <Row align="middle" justify="centre" gutter={16}>
           <Col span={8} offset={8}>
             <h1>Во что поиграть вместе?</h1>
-            <Button type="primary">Button</Button>
+            <div>{inputs}</div>
+            <Button type="primary">Найти общие игры</Button>
           </Col>
         </Row>
       </div>
