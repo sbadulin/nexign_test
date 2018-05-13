5a6e8b37201905947d448f961c4f6e124cc85db8
Sergey Badulin <sbadulin@gmail.com>
2018-05-14 00:22:14 +0300

    set individual values to fields

--- src/App.js
+++ src/App.js
@@ -1,27 +1,33 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Button, Row, Col, Input, Icon } from 'antd';
+import { Button, Row, Col, Input, Icon, Form } from 'antd';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      totalUsers: 3,
-      userName: ''
+      totalUsers: 3
     };
   }
-  emitEmpty = () => {
-    this.userNameInput.focus();
-    this.setState({ userName: '' });
-  };
+  // emitEmpty = () => {
+  //   this.userNameInput.focus();
+  //   this.setState({ userName: '' });
+  // };
   onChangeUserName = e => {
-    this.setState({ userName: e.target.value });
+    console.log(e.target.dataset.id);
+    this.setState({ ['userName' + e.target.dataset.id]: e.target.value });
   };
   onClickAddUser = e => {
-    this.setState({ totalUsers: this.state.totalUsers + 1 });
+    this.setState({
+      totalUsers: this.state.totalUsers + 1
+    });
+  };
+  onFormSUbmit = e => {
+    e.preventDefault();
+    console.log(e);
   };
   render() {
-    const { userName, totalUsers } = this.state;
+    const { totalUsers } = this.state;
     let inputs = [];
     for (let i = 0; i < totalUsers; i++) {
       let placeholderText = `Ссылка на пользователя ${i + 1}`;
@@ -29,10 +35,11 @@ class App extends Component {
         <Input
           placeholder={placeholderText}
           prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
-          value={userName}
+          value={this.state['userName' + i] || ''}
           onChange={this.onChangeUserName}
           ref={node => (this.userNameInput = node)}
           key={i}
+          data-id={i}
         />
       );
     }
@@ -41,12 +48,14 @@ class App extends Component {
         <Row align="middle" justify="centre" gutter={16}>
           <Col span={8} offset={8}>
             <h1>Во что поиграть вместе?</h1>
-            <div>{inputs}</div>
-            <h3 onClick={this.onClickAddUser}>
-              {<Icon type="plus-circle" style={{ color: 'rgba(0,0,0)' }} />}
-              Добавить пользователя
-            </h3>
-            <Button type="primary">Найти общие игры</Button>
+            <form onSubmit={this.onFormSubmit}>
+              <div>{inputs}</div>
+              <h3 onClick={this.onClickAddUser} style={{ cursor: 'pointer' }}>
+                {<Icon type="plus-circle" style={{ color: 'rgba(0,0,0)' }} />}
+                 Добавить пользователя
+              </h3>
+              <Button type="submit">Найти общие игры</Button>
+            </form>
           </Col>
         </Row>
       </div>
